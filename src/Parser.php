1d02b3d3bf6762f4c5893ddaa136d46f329b1b2b
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2016-02-24 15:43:41 +0100

    Removed Authority parsing from  to use  instead

--- src/Parser.php
+++ src/Parser.php
@@ -12,6 +12,7 @@ use Madkom\Uri\Authority\Host\IPv4;
 use Madkom\Uri\Authority\Host\IPv6;
 use Madkom\Uri\Authority\Host\Name;
 use Madkom\Uri\Authority\UserInfo;
+use Madkom\Uri\Parser\Authority;
 use Madkom\Uri\Scheme\Custom;
 use Madkom\Uri\Scheme\Http;
 use Madkom\Uri\Scheme\Https;
@@ -42,33 +43,8 @@ class Parser
         } else {
             throw new InvalidArgumentException("Malformed uri given, missing scheme in: {$uriString}");
         }
-        $userInfo = $iri->getUserInfo() ? UserInfo::createFromString($iri->getUserInfo()) : null;
-        if ($iri->getHost()) {
-            if (filter_var($iri->getHost(), FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-                $host = new IPv4($iri->getHost());
-            } elseif (0 === strpos($iri->getHost(), '[') &&
-                strlen($iri->getHost())-1 === strpos($iri->getHost(), ']') &&
-                filter_var($ipString = rtrim(ltrim($iri->getHost(), '['), ']'), FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
-            ) {
-                $host = new IPv6($ipString);
-            } else {
-                $host = new Name($iri->getHost());
-            }
-        } else {
-            $host = null;
-        }
-        if ($iri->getPort()) {
-            $port = $iri->getPort();
-        } elseif ($scheme instanceof NetworkScheme) {
-            $port = $scheme->getPort();
-        } else {
-            $port = null;
-        }
-        if (null !== $host) {
-            $authority = new Authority($host, $port, $userInfo);
-        } else {
-            $authority = null;
-        }
+        $authorityParser = new Authority();
+        $authority = $authorityParser->parse($iri->getAuthority());
         $path = Path::createFromString($iri->getPath());
         $query = Query::createFromString($iri->getQuery());
         $fragment = $iri->getFragment();
